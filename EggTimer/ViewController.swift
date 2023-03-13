4e6a767d50c2e849358979247dc2fb69ba13219e
Faiza Nawaz <Faizanawaz755@gmail.com>
2023-03-13 10:29:02 +0500

    practiced progressbar, audio player, datatypes, optionals

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,14 +7,66 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
-    let eggTime = ["Soft": 5, "Medium": 7, "Hard": 12]
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    var player: AVAudioPlayer!
 
+    let eggTime = ["Soft": 3, "Medium": 5, "Hard": 7]
+    var secondsPassed: Float = 0;
+    var totalTime: Float = 0
+    var timer = Timer()
+    
     @IBAction func hardnessSelected(_ sender: UIButton) {
         
         let hardness = sender.currentTitle!
-        print(eggTime[hardness] ?? "Error")
+        totalTime = Float((eggTime[hardness])!)
+        
+        reset()
+        startTimer()
     }
+    
+    fileprivate func reset() {
+        secondsPassed = 0;
+        progressBar.progress = 0
+        statusLabel.text = "Starting..."
+        timer.invalidate()
+        if(player != nil) {
+            player.stop()
+        }
+    }
+    
+    fileprivate func startTimer() {
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0,
+                                     target: self,
+                                     selector: #selector(updateTimer),
+                                     userInfo: nil,
+                                     repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        
+        if secondsPassed < totalTime {
+            
+            secondsPassed += 1
+            progressBar.progress = secondsPassed/totalTime
+        }
+        else {
+            statusLabel.text = "Done"
+            timer.invalidate()
+            playAlarm()
+        }
+    }
+    
+    fileprivate func playAlarm() {
+
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
+    
 }
